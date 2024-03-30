3079f4e7eabe972c1764ce93377c2a525d2510c8
Vitalya <vitamin11182005@gmail.com>
2024-03-30 17:12:10 +0300

    remove for cycle, remove suptitle

--- Work/Library/COLICH-KACHEST BOX&WHISKERS.py
+++ Work/Library/COLICH-KACHEST BOX&WHISKERS.py
@@ -5,4 +5,5 @@ df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
 
 # Построение категоризированной гистограммы box-and-whiskers (ящик с усами)
 df.boxplot(by='Страна', column='Средняя добыча за год (1000 баррелей/день)', grid=False)
+plt.suptitle('')
 plt.show()
--- Work/Library/COLICH-KACHESTV.py
+++ Work/Library/COLICH-KACHESTV.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dfs = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=None)
+df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
 
-for i, df in dfs.items():
-    grouped_data = df.groupby('Страна')['Средняя добыча за год (1000 баррелей/день)'].mean()
-    # Построение категоризированной гистограммы
-    plt.figure(figsize=(10, 6))
-    grouped_data.plot(kind='bar', color='skyblue')
-    plt.title('Среднегодовая добыча нефти по странам')
-    plt.xlabel('Страна')
-    plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
-    plt.xticks(rotation=45)
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    plt.show()
+grouped_data = df.groupby('Страна')['Средняя добыча за год (1000 баррелей/день)'].mean()
+# Построение категоризированной гистограммы
+plt.figure(figsize=(10, 6))
+grouped_data.plot(kind='bar', color='skyblue')
+plt.title('Среднегодовая добыча нефти по странам')
+plt.xlabel('Страна')
+plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
