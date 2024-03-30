7bef2a6d2c9cf746fc64dcad64a83dcdd4cce2f1
Vitalya <vitamin11182005@gmail.com>
2024-03-30 17:24:21 +0300

    edit kachest-kachest

--- Work/Library/KACHEST-KACHESTV.py
+++ Work/Library/KACHEST-KACHESTV.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
 
-# Создаем новый столбец, содержащий информацию о номере страны в рейтинге и названии страны
-df['Рейтинг и страна'] = df['Номер страны по добыче'].astype(str) + '. ' + df['Страна']
-
-# Создание кластеризованной столбчатой диаграммы
+grouped_data = df.groupby('Страна')['Номер страны по добыче'].mean()
 plt.figure(figsize=(10, 6))
-plt.bar(df['Рейтинг и страна'], df['Номер страны по добыче'])
+grouped_data.plot(kind='bar')
 plt.title('Кластеризованная столбчатая диаграмма')
 plt.xlabel('Название страны')
 plt.ylabel('Номер страны в рейтинге')
-plt.xticks(rotation=90)
+plt.xticks(rotation=0)
+plt.yticks(list(grouped_data))
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
