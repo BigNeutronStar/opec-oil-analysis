ef224487e7d7aeccf82e53ae6b74807c8a65b2f2
Vitalya <vitamin11182005@gmail.com>
2024-04-03 22:26:52 +0300

    artem dodelay

--- Work/Library/COLICH-KACHEST BOX&WHISKERS.py
+++ Work/Library/COLICH-KACHEST BOX&WHISKERS.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
 
 # Построение категоризированной гистограммы box-and-whiskers (ящик с усами)
-df.boxplot(by='Страна', column='Средняя добыча за год (1000 баррелей/день)', grid=False)
+df.boxplot(by='Страна', column='Добыча (1000 баррелей/день)', grid=True)
 plt.suptitle('')
+
+df.boxplot(by='Номер страны по добыче', column='Добыча (1000 баррелей/день)', grid=True)
+plt.suptitle('')
+
 plt.show()
--- Work/Library/COLICH-KACHESTV.py
+++ Work/Library/COLICH-KACHESTV.py
@@ -3,14 +3,23 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
 
-grouped_data = df.groupby('Страна')['Средняя добыча за год (1000 баррелей/день)'].mean()
+grouped_data1 = df.groupby('Страна')['Средняя добыча за год (1000 баррелей/день)'].mean()
+grouped_data2 = df.groupby('Номер страны по добыче')['Средняя добыча за год (1000 баррелей/день)'].mean()
 # Построение категоризированной гистограммы
 plt.figure(figsize=(10, 6))
-grouped_data.plot(kind='bar', color='skyblue')
+grouped_data1.plot(kind='bar')
 plt.title('Среднегодовая добыча нефти по странам')
 plt.xlabel('Страна')
 plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
 plt.xticks(rotation=45)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.tight_layout()
+
+plt.figure(figsize=(10, 6))
+grouped_data2.plot(kind='bar')
+plt.title('Среднегодовая добыча нефти по странам')
+plt.xlabel('Номер страны по добыче')
+plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
 plt.show()
