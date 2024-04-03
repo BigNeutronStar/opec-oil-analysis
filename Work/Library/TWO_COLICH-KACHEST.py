9588f2138abc8cbede54b5d36eccf82c3746d76d
Vitalya <vitamin11182005@gmail.com>
2024-04-04 00:44:13 +0300

    git commit -m

--- Work/Library/TWO_COLICH-KACHEST.py
+++ Work/Library/TWO_COLICH-KACHEST.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
 
 x = df['Цена за баррель']
-y = df['Средняя добыча за год (1000 баррелей/день)']
+y = df['Среднедневная добыча за период (1000 баррелей/день)']
 
 country_numbers = df['Номер страны по добыче'].unique()
 
 plt.figure(figsize=(10, 6))
 for country_number in country_numbers:
     subset = df[df['Номер страны по добыче'] == country_number]
-    plt.scatter(subset['Цена за баррель'], subset['Средняя добыча за год (1000 баррелей/день)'], label=country_number)
+    plt.scatter(subset['Цена за баррель'], subset['Среднедневная добыча за период (1000 баррелей/день)'], label=country_number)
 plt.title('Категоризированная диаграмма рассеивания:')
 plt.xlabel('Цена за баррель')
-plt.ylabel('Средняя добыча за год (1000 баррелей/день)')
+plt.ylabel('Среднедневная добыча за период (1000 баррелей/день)')
 plt.legend(title='Номер страны по добыче')
 plt.grid(True)
 plt.show()
