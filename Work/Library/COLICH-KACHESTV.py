dc147dd47ab29435ee1d6e174e12056a89fbb427
Vitalya <vitamin11182005@gmail.com>
2024-04-03 23:14:21 +0300

    artem dodelay

--- Work/Library/COLICH-KACHESTV.py
+++ Work/Library/COLICH-KACHESTV.py
@@ -2,24 +2,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_excel('./Work/Data/nf_db.xlsx', sheet_name=0)
+data_course = pd.read_excel('./Work/Data/курс рубля.xlsx')
 
-grouped_data1 = df.groupby('Страна')['Средняя добыча за год (1000 баррелей/день)'].mean()
-grouped_data2 = df.groupby('Номер страны по добыче')['Средняя добыча за год (1000 баррелей/день)'].mean()
+country_dobych = df.groupby('Страна')['Средняя добыча за год (1000 баррелей/день)'].mean()
+number_dobych = df.groupby('Номер страны по добыче')['Средняя добыча за год (1000 баррелей/день)'].mean()
+
+data_course.plot(x='Date', y='Price', kind='line')
+
 # Построение категоризированной гистограммы
-plt.figure(figsize=(10, 6))
-grouped_data1.plot(kind='bar')
-plt.title('Среднегодовая добыча нефти по странам')
-plt.xlabel('Страна')
-plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
-plt.xticks(rotation=45)
-plt.grid(axis='y', linestyle='--', alpha=0.7)
 
-plt.figure(figsize=(10, 6))
-grouped_data2.plot(kind='bar')
-plt.title('Среднегодовая добыча нефти по странам')
-plt.xlabel('Номер страны по добыче')
-plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
-plt.xticks(rotation=45)
-plt.grid(axis='y', linestyle='--', alpha=0.7)
+# country_dobych.plot(kind='bar')
+# plt.title('Среднегодовая добыча нефти по странам')
+# plt.xlabel('Страна')
+# plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
+# plt.xticks(rotation=45)
+# plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+
+# number_dobych.plot(kind='bar')
+# plt.title('Среднегодовая добыча нефти по странам')
+# plt.xlabel('Номер страны по добыче')
+# plt.ylabel('Средняя добыча нефти (1000 баррелей/день)')
+# plt.xticks(rotation=45)
+# plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.show()
