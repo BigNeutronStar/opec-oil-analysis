2ac180dc4a43bf4b0b654d68f2795aa36d36e33d
Vitalya <vitamin11182005@gmail.com>
2024-04-08 01:11:12 +0300

    bd generation edit

--- Work/Library/script.py
+++ Work/Library/script.py
@@ -1,14 +1,32 @@
 import pandas as pd
 prod = pd.read_excel('./Work/Data/oil-production_db.xlsx')
-dates = pd.read_excel('./Work/Data/db.xlsx', sheet_name=1)['Дата']
+date_id = pd.read_excel('./Work/Data/db.xlsx', sheet_name=1)
 bd = pd.read_excel('./Work/Data/db_test.xlsx')
-countries = prod['Страна']
-res = []
-# f = open('./Work/Data/file.txt', 'w')
-for c in countries:
-    for i in range(len(dates)):
-        # f.write(c)
-        # f.write('\n')
-        res.append(c)
-bd['Страна'] = res
+
+dates = list(pd.to_datetime(date_id['Дата'], format='%m.%d.%Y'))
+price = list(date_id['Цена за баррель'])
+curr = list(date_id['Курс рубля'])
+countries = list(prod['Страна'])
+
+
+dates_count = len(dates)
+country_count = len(countries)
+
+bd['date_id'] = list(range(dates_count)) * country_count
+bd['Дата'] = dates * country_count
+bd['Цена за баррель'] = price * country_count
+bd['Курс рубля'] = curr * country_count
+
+temp = list()
+temp_id = list()
+for c, i in countries:
+        temp_id += [i] * dates_count
+        temp += [c] * dates_count
+bd['country_id'] = temp_id
+bd['Страна'] = temp
+
+# bd['Номер страны по добыче'] = []
+# bd['Среднедневная добыча за год (1000 бар/д)'] = []
+
+
 bd.to_excel('./Work/Data/db_test.xlsx')
