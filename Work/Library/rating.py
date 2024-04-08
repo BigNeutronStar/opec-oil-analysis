2be8aaf4ec04893c9bfa191e4789692a4f65a163
Vitalya <vitamin11182005@gmail.com>
2024-04-08 17:59:51 +0300

    database_filler

---
+++ Work/Library/rating.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+bd = pd.read_excel('./Work/Data/oil-production_db.xlsx')
+prod = {}
+rating = {}
+countries = bd['Страна']
+
+def form():
+    for i in range(len(countries)):
+        row = bd.iloc[i]
+        sum = round(pd.to_numeric(row, errors='coerce').sum(), 3)
+        prod[countries[i]] = sum
+        rating[countries[i]] = 0
+
+    prod = dict(sorted(prod.items(), key=lambda item: item[1], reverse=True))
+
+    i = 1
+    for c in prod.keys():
+        rating[c] = i
+        i += 1
+
+def get_rating(country: str):
+    return rating[country]
+
+def get_production(country: str):
+    return prod[country]
+
