7c95d5e501438ab9ab9d4d9be18bdd35e391427b
Vitalya <vitamin11182005@gmail.com>
2024-04-14 01:10:43 +0300

    edit

--- Work/Library/paths.py
+++ Work/Library/paths.py
@@ -1,14 +1,14 @@
-output_dir = "Work/Data/generated_data"
+output_dir = "./Data/generated_data"
 
 output_paths = {
-    "main":"Work/Data/generated_data/db.xlsx",
-    "date":"Work/Data/generated_data/date_db.xlsx",
-    "rating":"Work/Data/generated_data/rating_db.xlsx",
-    "dailyprd":"Work/Data/generated_data/daily-production_db.xlsx",
+    "main":"./Data/generated_data/db.xlsx",
+    "date":"./Data/generated_data/date_db.xlsx",
+    "rating":"./Data/generated_data/rating_db.xlsx",
+    "dailyprd":"./Data/generated_data/daily-production_db.xlsx",
 }
 
 public_paths = {
-    "prd":"Work/Data/public_data/oil-production_db.xlsx",
-    "price":"Work/Data/public_data/oil-price_db.xlsx",
-    "curr":"Work/Data/public_data/currency-pair_db.xlsx",
+    "prd":"./Data/public_data/oil-production_db.xlsx",
+    "price":"./Data/public_data/oil-price_db.xlsx",
+    "curr":"./Data/public_data/currency-pair_db.xlsx",
 }
