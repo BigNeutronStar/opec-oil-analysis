8f451b2f6da141faf9ac5328413e1e2fd63315d1
Vitalya <vitamin11182005@gmail.com>
2024-05-25 19:37:07 +0300

    add main to global vars

--- Work/Library/data.py
+++ Work/Library/data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-
-countries = dates = daily_production = pd.DataFrame()
+__all__ = ['countries', 'dates', 'daily_production', 'main']
+countries = dates = daily_production = main = pd.DataFrame()
 
 def read_data(databases_paths):
     global countries, dates, daily_production
@@ -10,6 +10,7 @@ def read_data(databases_paths):
     daily_production = pd.read_excel(databases_paths['daily_production'])
 
 def collect_to_main(path):
+    global main
     main = pd.merge(daily_production, dates, on='date_id')
     main = pd.merge(main, countries, on='country_id')
     main.to_excel(path, index=False)
--- Work/main.py
+++ Work/main.py
@@ -1,6 +1,7 @@
 import sys
 from Scripts import data_collector
-#from Scripts import graphics_generator
+from Scripts import report_generator
+##from Scripts import graphics_generator
 for param in sys.argv[1:]:
         if param == '-g':
                 data_collector.generate_main()
