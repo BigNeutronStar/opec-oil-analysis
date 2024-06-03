5c6e867880c5d92a4d345d08a6ea3a9aeee593a1
ARTEM <artem@MacBook-Pro-ARTEM-4.local>
2024-06-03 22:50:56 +0300

    добавил page_handler.py

---
+++ Work/Scripts/page_handler.py
@@ -0,0 +1,43 @@
+from math import pi
+import time
+import flet as ft
+from flet_route import Routing, path
+
+from Library.pages import Home
+from Library.pages import Graphics
+from Library.pages import Reports
+from Library.pages import Info
+from Library.pages import Loading
+from Library.pages import ViewData
+
+from Scripts import data_collector
+
+def setup_page(page: ft.Page):
+    page.theme_mode='dark'
+    page.window_maximized = True
+    page.window_title_bar_hidden = True
+    page.window_title_bar_buttons_hidden = True
+    page.window_min_height = 800
+    page.window_min_width = 700
+    page.spacing = 0
+    page.padding = 0
+
+    app_routes = [
+        path(url = "/", clear = True, view=Loading),
+        path(url = "/home", clear = True, view=Home),
+        path(url = "/graphics", clear=True, view=Graphics),
+        path(url = "/info", clear=True, view=Info),
+        path(url = "/view_data", clear=True, view=ViewData),
+        path(url = "/reports", clear=True, view=Reports),
+    ]
+
+    Routing(page=page,
+            app_routes=app_routes)
+
+def run_app(page: ft.Page):
+    page.go(page.route)
+    data_collector.read_data()
+    page.go('/home')
+    
+     
+    
