5a51e18d63d2e16147445c7210ab68f3b4d4a5a4
Vitalya <vitamin11182005@gmail.com>
2024-06-04 14:31:54 +0300

    e

--- Work/main.py
+++ Work/main.py
@@ -1,6 +1,6 @@
 import sys
 from Scripts import data_collector
-#from Scripts import graphics_generator
+
 for param in sys.argv[1:]:
         if param == '-g':
                 data_collector.generate()
